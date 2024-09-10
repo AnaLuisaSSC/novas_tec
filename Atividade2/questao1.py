30ee1e266cd0152d28fc2a757043a7c7cbf71025
Ana Luísa Santos Corrêa <111926916+AnaLuisaSSC@users.noreply.github.com>
2024-09-10 12:49:13 -0300

    Update questao1.py

--- Atividade2/questao1.py
+++ Atividade2/questao1.py
@@ -1,8 +1,8 @@
 '''
 1- O fatorial de um inteiro não negativo n é escrito como n! (pronuncia-se “n fatorialˮ) e é definido
 como segue:
-n!  n · (n - 1) · (n - 2) · ... · 1 (para valores de n maiores ou iguais a 1) e n!  1 (para n  0)
-Por exemplo, 5!  5 · 4 · 3 · 2 · 1, o que dá 120
+n! = n · (n - 1) · (n - 2) · ... · 1 (para valores de n maiores ou iguais a 1) e n!  1 (para n  0)
+Por exemplo, 5! = 5 · 4 · 3 · 2 · 1, o que dá 120
 
 Escreva um aplicativo que lê um inteiro não negativo, calcula e imprime seu fatorial
 '''
@@ -15,10 +15,8 @@ def fatorial_iterativo(n):
         resultado *= i
     return resultado
 
-# Exemplo de uso
 n = int(input("Digite um número para calcular o fatorial: "))
 print(f"O fatorial de {n} é {fatorial_iterativo(n)}")
-
 
 def fatorial_recursivo(n):
     if n < 0:
@@ -28,6 +26,5 @@ def fatorial_recursivo(n):
     else:
         return n * fatorial_recursivo(n - 1)
 
-# Exemplo de uso
 n = int(input("Digite um número para calcular o fatorial: "))
 print(f"O fatorial de {n} é {fatorial_recursivo(n)}")
