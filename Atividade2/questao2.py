32f33211eee4edc472c802ff074feb2a7e33eb32
Ana Luísa Santos Corrêa <111926916+AnaLuisaSSC@users.noreply.github.com>
2024-09-10 12:50:47 -0300

    Update questao2.py

--- Atividade2/questao2.py
+++ Atividade2/questao2.py
@@ -3,7 +3,7 @@
 seguidos de um dígito verificador, calculado conforme exemplificado a seguir. Dado um número
 de conta n, exiba o número de conta completo correspondente. Seja n = 7314 o número da
 conta. Adicionamos os dígitos de n e obtemos a soma s = 4+1+3+7 = 15; Calculamos o resto
-da divisão de s por 10 e obtemos o dígito d  5. Número de conta completo: 007314-5
+da divisão de s por 10 e obtemos o dígito d = 5. Número de conta completo: 007314-5
 '''
 
 def calcular_digito_verificador(numero_conta):
@@ -21,7 +21,6 @@ def calcular_digito_verificador(numero_conta):
     
     return numero_conta_completo
 
-# Exemplo de uso
 try:
     numero_conta = int(input("Digite o número da conta (até 6 dígitos): "))
     print(f"Número da conta completo: {calcular_digito_verificador(numero_conta)}")
