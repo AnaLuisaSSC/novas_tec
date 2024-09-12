967daa4df1c41eece15d41ec15f4de8aceceafc8
Ana Luísa Santos Corrêa <111926916+AnaLuisaSSC@users.noreply.github.com>
2024-09-12 18:11:55 -0300

    commit

--- Atividade2/questao7.py
+++ Atividade2/questao7.py
@@ -3,39 +3,30 @@
 aproximação abaixo:
         cos (x) = 1 - x²/2! + x⁴/4! - x{x}^{6}/6! + ...
 '''
-import math 
 
-def calcular_cos_taylor(x, termos=10)
-'''
-param x: Valor para o qual calcular o cosseno.
-param termos: Número de termos a usar na série de Taylor.
-return: Valor aproximado de cos(x).
-'''
+xI = int(input("Qual valor de x para cos(x): "))
+x = xI * 3.1415 / 180
 
-soma = 0
+cos = 0
+n = 0
 
-for n in range(termos):
-        termos = ((-1) ** n) * (x ** (2 * n)) / math.factorial(2 * n)
-        soma += termo
-    
-    return soma
+for i in range(0, 1000, 2):
 
-def main():
+    fatorial = 1
+    for num in range(1, i + 1):
+        fatorial *= num
 
-x = float(input("Digite o valor de x (em radianos): "))
-    
-    termos = int(input("Digite o número de termos para a aproximação: "))
-    
-    # calcula o valor aproximado de cos(x)
-    resultado = calcular_cos_taylor(x, termos)
-    
-    # resultado
-    print(f"O valor aproximado de cos({x}) usando {termos} termos é: {resultado:.6f}")
+    termo = ((-1) ** n) * (x ** i) / fatorial
+    cos += termo
 
-if __name__ == "__main__":
-    main()
+    n += 1
+
+print(f"cos({xI}) = {cos}")
+
 
 '''
+codigo do professor
+
 xI = int(input("qual valor de x para cos(x):"))
 x = xI*3.1415/180
 
