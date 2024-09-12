e80f8535b13d458c974dfb13f828eb2837c19f47
Ana Luísa Santos Corrêa <111926916+AnaLuisaSSC@users.noreply.github.com>
2024-09-12 18:56:43 -0300

    commit

--- Atividade2/questao1.py
+++ Atividade2/questao1.py
@@ -1,40 +1,20 @@
 '''
 1- O fatorial de um inteiro não negativo n é escrito como n! (pronuncia-se “n fatorialˮ) e é definido
 como segue:
-n! = n · (n - 1) · (n - 2) · ... · 1 (para valores de n maiores ou iguais a 1) e n!  1 (para n  0)
-Por exemplo, 5! = 5 · 4 · 3 · 2 · 1, o que dá 120
+n! = n · (n - 1) · (n - 2) · ... · 1 (para valores de n maiores ou iguais a 1) e n! = 1 (para n = 0)
+Por exemplo, 5! = 5 · 4 · 3 · 2 · 1, o que da 120
 
 Escreva um aplicativo que lê um inteiro não negativo, calcula e imprime seu fatorial
 '''
-def fatorial_iterativo(n):
-    if n < 0:
-        raise ValueError("O fatorial não está definido para números negativos.")
-    
-    resultado = 1
-    for i in range(1, n + 1):
-        resultado *= i
-    return resultado
 
-n = int(input("Digite um número para calcular o fatorial: "))
-print(f"O fatorial de {n} é {fatorial_iterativo(n)}")
-
-def fatorial_recursivo(n):
-    if n < 0:
-        raise ValueError("O fatorial não está definido para números negativos.")
-    if n == 0:
-        return 1
-    else:
-        return n * fatorial_recursivo(n - 1)
-
-n = int(input("Digite um número para calcular o fatorial: "))
-print(f"O fatorial de {n} é {fatorial_recursivo(n)}")
-
-'''
 num = int(input("digite um numero"))
+
 numI = num    # numInicial
+
 fatorial = 1
-while(num>=1):
-    fatorial = fatorial*num;
-    num=num-1;
+
+ while(num>=1):
+    fatorial *= num
+    num -= num
 print(f"o fatorial de {num}! = {fatorial}.")
-'''
+
