91d0cbcf93272b48ce6bc4157681b977c85292e7
AnaLuisaSSC <analuisasantoscorrea@gmail.com>
2024-09-12 11:00:13 -0300

    commit

---
+++ 12092024/funcoes.py
@@ -0,0 +1,18 @@
+'''
+__ = tentativa de encapsular, de ser privado
+help(range.count)
+
+aspas triplas (' ' ' ) funciona como comentario e documentacao
+
+exemplo de funcao:
+
+def fat(num):
+    fatorial = 1
+
+    while(num>=1);
+        fatorial = fatorial*num
+        num=num-1
+    return fatorial
+print(fat.__doc__)
+print(f"o fatorial de numnumI! ={fat(num).}")
+'''
