7b581045aa97569b02bf544c2afd9bffa6ef24fb
Ana Luísa Santos Corrêa <111926916+AnaLuisaSSC@users.noreply.github.com>
2024-09-17 19:11:46 -0300

    commit

--- Atividade3/questao2.py
+++ Atividade3/questao2.py
@@ -4,7 +4,27 @@ função deve retornar uma string com a data formatada no formato "dd/mm/aaaa". 
 os parâmetros sejam passados com seus respectivos nomes e em qualquer ordem. Teste a
 função com diferentes combinações de argumentos nomeados.
 '''
-def formatar_data(d, m, a):
-    if d <= 1 or m <= 1 or a <= 1:
-        print("datas nao podem ser: 0 ou negativas")
+def formatar_data(dia, mes, ano):
+    if not (1 <= dia <= 31) or not (1 <= mes <= 12) or ano <= 0:
+        print("datas invalida")
         return
+    dia_formatado = f"{dia:02d}"  # sempre deve ter 2 digitos
+    mes_formatado = f"{mes:02d}"  # sempre deve ter 2 digitos
+    ano_formatado = f"{ano:04d}"  # sempre deve ter 4 digitos
+
+    return f"{dia_formatado}/{mes_formatado}/{ano_formatado}"
+
+def dados_usuario():
+    try:
+        dia = int(input("Digite o dia: "))
+        mes = int(input("Digite o mês: "))
+        ano = int(input("Digite o ano: "))
+        
+        # formata a data 
+        data_formatada = formatar_data(dia, mes, ano)
+        print(f"Data formatada: {data_formatada}")
+    except ValueError as e:
+        print(f"Erro: {e}")
+
+# chama a função 
+dados_usuario()
