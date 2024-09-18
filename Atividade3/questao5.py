34566be8e48fa9d741b089f8cd40a1cfaaebd2b6
Ana Luísa Santos Corrêa <111926916+AnaLuisaSSC@users.noreply.github.com>
2024-09-17 21:40:14 -0300

    commit

--- Atividade3/questao5.py
+++ Atividade3/questao5.py
@@ -1,4 +1,13 @@
 '''
-5- 
+5- Para cada problema a seguir defina uma função recursiva, faça a simulação por substituição e
+desenhe o fluxo de chamadas e retornos:
+a. Calcular o fatorial de um número natural.
+b. Calcular a potencia de um número, dado um expoente.
+c. Calcular o resto da divisão inteira usando subtração.
+d. Calcular o quociente da divisão inteira usando subtração.
+e Calcular o produto de dois naturais usando adição.
+f Calcular a soma de dois naturais usando as funções suc(n) e pred(n) que devolvem,
+respectivamente, o sucessor e o predecessor de um natural n.
+
 '''
 
--- Atividade3/questao7.py
+++ Atividade3/questao7.py
@@ -1,3 +1,5 @@
 '''
-
+7- Implemente uma função chamada calcular_estatisticas que recebe uma quantidade indefinida
+de números e retorna três valores: a média, o valor máximo e o valor mínimo da lista. Teste a
+função com diferentes listas de números e imprima os resultados retornados.
 '''
