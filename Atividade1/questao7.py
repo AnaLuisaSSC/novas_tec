4058c9b900d0cd02227e49410c9a27acd02d9891
Ana Luísa Santos Corrêa <111926916+AnaLuisaSSC@users.noreply.github.com>
2024-09-23 16:50:14 -0300

    Update questao7.py

--- Atividade1/questao7.py
+++ Atividade1/questao7.py
@@ -3,6 +3,12 @@
 essa conversão é:
 F =  9/5C + 32
 '''
+c = float(input('Escreva um valor para temperatura: '))
+
+f = ((9/5)*c)+32
+print('Celsius: {} \nfahrenheit: {} '.format(c,f))
+#pode ser :   print(f'Celsius: {f} \nfahrenheit: {f} ')
+'''
 def converter_temperatura(celsius):
     # calculo de converter
     fahrenheit = (9/5) * celsius + 32
@@ -20,7 +26,7 @@ def main():
 
 if __name__ == "__main__": 
     main()
-
+'''
 '''
  if __name__ == "__main__": é uma verificação que garante que o código dentro desse bloco só será executado se o script for executado 
  diretamente, e não quando for importado como um módulo em outro script.
