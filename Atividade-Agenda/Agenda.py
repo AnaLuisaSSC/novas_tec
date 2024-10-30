16bd4c7b550dfde0d225ea27e1570f5be3bd8b1b
Ana Luísa Santos Corrêa <111926916+AnaLuisaSSC@users.noreply.github.com>
2024-10-30 16:05:58 -0300

    commit

---
+++ Atividade-Agenda/Agenda.py
@@ -0,0 +1,13 @@
+def dicionario_listas():  #criar_dicionario_com_listas
+    dicionario = {}
+   
+    while True:
+        .
+def apaga()
+altera()
+lista()
+le()
+grava()
+menu()
+
+verificarbirthday()
